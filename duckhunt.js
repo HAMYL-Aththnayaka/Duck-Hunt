c269764a2b05b3d5b40802bb1781f6b4ceaaec58
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-08-26 23:16:21 +0530

    New animation added"

--- duckhunt.js
+++ duckhunt.js
@@ -1,24 +1,25 @@
 let ducks;
 let duckCount = 1;
 
-let duckImageNames =["./assets/duck-left.gif","./assets/duck-right.gif"]
+let duckImageNames = ["./assets/duck-left.gif", "./assets/duck-right.gif"]
 let duckImageWidth = 96;
 let duckImageHeight = 93;
 
 let gameWidth = window.screen.width;
-let gameHeight = window.screen.height*3/4;
+let gameHeight = window.screen.height * 3 / 4;
 
 let duckVelocityX = 5;
 let duckVelocityY = 5;
 
-window.onload = function() {
+window.onload = function () {
     addDucks();
+    setInterval(moveDucks, 1000 / 60);
 }
 
 function addDucks() {
-    ducks =[];
-    for(let i = 0; i< duckCount; i++){
-        let duckImageName = duckImageNames[Math.floor(Math.random()*3)];
+    ducks = [];
+    for (let i = 0; i < duckCount; i++) {
+        let duckImageName = duckImageNames[Math.floor(Math.random() * 2)];
 
         //image
         let duckImage = document.createElement("img");
@@ -32,16 +33,16 @@ function addDucks() {
 
         let duck = {
             image: duckImage,
-            x:randomPosition(gameWidth - duckImageWidth),
-            y:randomPosition(gameHeight - duckImageHeight),
+            x: randomPosition(gameWidth - duckImageWidth),
+            y: randomPosition(gameHeight - duckImageHeight),
             velocityX: duckVelocityX,
             velocityY: duckVelocityY
         }
-        duck.image.style.left =String(duck.x) + "px"; // x position 
+        duck.image.style.left = String(duck.x) + "px"; // x position 
         duck.image.style.top = String(duck.y) + "px"; // y position
 
 
-        if(duck.image.src.includes("left")){
+        if (duck.image.src.includes("left")) {
             duck.velocityX = -duckVelocityX;
         } else {
             duck.velocityX = duckVelocityX;
@@ -49,8 +50,29 @@ function addDucks() {
 
         ducks.push(duck);
     }
+}
 
-    function randomPosition(limit){
-        return Math.floor(Math.random() * limit);
+function randomPosition(limit) {
+    return Math.floor(Math.random() * limit);
+}
+
+function moveDucks() {
+    for (let i = 0; i < ducks.length; i++) {
+        let duck = ducks[i];
+
+        duck.x += duck.velocityX;
+        if(duck.x < 0 || duck.x + duckImageWidth > gameWidth ) {
+            //duck.velocityX -= duck.velocityX;
+            duck.velocityX *= -1;
+        }
+        duck.y += duck.velocityY;
+        if(duck.y < 0 || duck.y + duckImageHeight > gameHeight ) {
+            //duck.velocityY -= duck.velocityY;
+            duck.velocityY *= -1;
+        }
+
+        duck.image.style.left = String(duck.x) + "px"; // x position 
+        duck.image.style.top = String(duck.y) + "px"; // y position
+
     }
 }
